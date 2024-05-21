6ec49ef28ccf45eb3d605d7f5b2eb6afa6392454
wannabehime <aqr.kai.4@gmail.com>
2024-05-21 14:41:09 +0900

    Succeeded in getting current location

--- chainstoreapp/src/main/resources/static/calcroute.js
+++ chainstoreapp/src/main/resources/static/calcroute.js
@@ -1,17 +1,22 @@
-function calcRoute() {
-	let directionsService = new google.maps.DirectionsService();
-	let directionsRenderer = new google.maps.DirectionsRenderer();
-	// #TODO: 現在地と目的地を緯度経度で渡す
+//#TODO: htmlの方の更新を反映させる必要
+
+document.querySelectorAll('.btn').forEach(element => element.addEventListener('click', function(){calcRoute(this)})); //ルート検索ボタンに検索関数を結び付け
+function calcRoute(obj) {
+	let lat = obj.previousElementSibling.value;
+	let lng = obj.previousElementSibling.getAttribute('name');
+	let latLng = new google.maps.LatLng(lat, lng);
+	//alert(latLng);
 	let request = {
-		origin: {lat: 35.681236, lng: 139.767125},
-		destination: {query: '秋葉原駅'},
+		origin: {lat: 35.681236, lng: 139.767125}, 	// #TODO: 現在地と目的地を緯度経度で渡す
+		destination: latLng,
 		travelMode: 'WALKING'
 	};
-	directionsService.route(request, function(result, status) {
+	new google.maps.DirectionsService().route(request, function(result, status) { //第一引数をリクエストすると返ってくるresultとstatusを第二引数の関数に渡す
 		if (status === 'OK') {
-			directionsRenderer.setDirections(result);
+			//alert(status);
+			directionsRenderer.setDirections(result); //ルートをマップに表示
 		} else {
-			alert('Directions request failed due to ' + status);
+			alert(status);
 		}
 	});
 }
