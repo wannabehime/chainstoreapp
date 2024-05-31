6d5d2cd25a3850e7b921bc53a71f46dcc6ad99fe
wannabehime <aqr.kai.4@gmail.com>
2024-05-31 12:01:18 +0900

    completed asynchronous processing

--- chainstoreapp/src/main/java/com/example/chainstoreapp/repository/StationMapper.java
+++ chainstoreapp/src/main/java/com/example/chainstoreapp/repository/StationMapper.java
@@ -1,15 +1,15 @@
-package com.example.chainstoreapp.repository;
-
-import java.util.ArrayList;
-
-import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Param;
-
-import com.example.chainstoreapp.entity.Station;
-
-@Mapper
-public interface StationMapper {
-	
-	ArrayList<Station> selectByStation_Name(@Param("searchTerm") String searchTerm, @Param("name") String name, @Param("prefix") String prefix);
-
-}
+//package com.example.chainstoreapp.repository;
+//
+//import java.util.ArrayList;
+//
+//import org.apache.ibatis.annotations.Mapper;
+//import org.apache.ibatis.annotations.Param;
+//
+//import com.example.chainstoreapp.entity.Station;
+//
+//@Mapper
+//public interface StationMapper {
+//	
+//	ArrayList<Station> selectByStation_Name(@Param("searchTerm") String searchTerm, @Param("name") String name, @Param("prefix") String prefix);
+//
+//}
--- chainstoreapp/src/main/java/com/example/chainstoreapp/service/impl/StationServiceImpl.java
+++ chainstoreapp/src/main/java/com/example/chainstoreapp/service/impl/StationServiceImpl.java
@@ -4,8 +4,6 @@ import java.util.ArrayList;
 
 import org.springframework.stereotype.Service;
 
-import com.example.chainstoreapp.entity.Station;
-import com.example.chainstoreapp.repository.StationMapper;
 import com.example.chainstoreapp.service.StationService;
 
 import lombok.RequiredArgsConstructor;
@@ -16,19 +14,19 @@ import lombok.RequiredArgsConstructor;
 public class StationServiceImpl implements StationService {
 
 //	MapperのDI
-	private final StationMapper stationMapper;
+//	private final StationMapper stationMapper;
 	
 	@Override
 //	駅名データベースから、駅名の一部を手掛かりに駅名リストを取得
 	public ArrayList<String> getStationNames(String term) {
 		
-		ArrayList<Station> stations = stationMapper.selectByStation_Name("%" + term + "%", term + "駅", term + "%");
+//		ArrayList<Station> stations = stationMapper.selectByStation_Name("%" + term + "%", term + "駅", term + "%");
 		
 		ArrayList<String> stationNames = new ArrayList<>();
-		for(Station station : stations) {
-			stationNames.add(station.getName_with_prefecture());
-		}
-//		stationNames.add(0, "武蔵小金井駅");
+//		for(Station station : stations) {
+//			stationNames.add(station.getName_with_prefecture());
+//		}
+		stationNames.add(0, "武蔵小金井駅");
 		
 		return stationNames;
 	}
