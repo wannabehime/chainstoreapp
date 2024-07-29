8f46966dca3e7f213718cba5ee9aedd28dd10fb4
wannabehime <aqr.kai.4@gmail.com>
2024-07-29 11:35:30 +0900

    repaired initMap function

--- chainstoreapp/src/main/resources/static/js/admin.js
+++ chainstoreapp/src/main/resources/static/js/admin.js
@@ -1,82 +1,79 @@
 //		====== 地図の初期化 ======
 var directionsRenderer;
-let currentLatLng;		
+let currentLatlng;		
 let map;
 let markers = [];
 let bounds;
+let currentLocationMarker;
+let currentLocationCircle;
+const currentLocationInfoStatusDiv = document.getElementById('current-location-information-status');
 
 function initMap() {
-	let currentMarker;
 	directionsRenderer = new google.maps.DirectionsRenderer();
-	
-	function success(position) {
-		currentLatLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude); // 現在地のlatlngオブジェクトを格納
-		
-		document.getElementById('current-latlng').value = currentLatLng;  //店舗検索の中心として、フォームのhiddenで送る現在地の更新
-		
-		if(typeof map === 'undefined'){
-			let mapOptions = {
-				zoom: 15,
-				center: currentLatLng,
-				mapId: "574de86c3981bebd"
-			};
-			// TODO: mapoptionはmapIdだけでいい？
-			map = new google.maps.Map(document.getElementById('map'), mapOptions); 	// マップがまだ存在しない場合は新しく作成
-		}
-				
-		if (typeof currentMarker === 'undefined') { // マーカーがまだ存在しない場合は新しく作成
-			const iconDiv = document.createElement('div');
-			iconDiv.style.backgroundColor = '#115EC3';
-			iconDiv.style.border = '2px solid white';
-			iconDiv.style.borderRadius = '50%';
-			iconDiv.style.width = '18px';
-			iconDiv.style.height = '18px';
-			iconDiv.style.boxShadow = '0 0 6px rgba(0, 0, 0, 0.5)';
-			
-			currentMarker = new google.maps.marker.AdvancedMarkerView({
-                map,
-				position: currentLatLng,
-				content: iconDiv,
-    		});
-			
-			new google.maps.Circle({
-	            strokeColor: '#115EC3',
-	            strokeOpacity: 0.2,
-	            strokeWeight: 1,
-	            fillColor: '#115EC3',
-	            fillOpacity: 0.2,
-	            map: map,
-	            center: currentLatLng,
-	            radius: 40
-	        });   
-		} else { 							// マーカーが存在する場合は位置を更新
-			currentMarker.position = currentLatLng;
-		}
-		
-	}
-
-	function fail(error) {
-		alert('位置情報の取得に失敗しました。エラーコード：' + error.code);
-		if(typeof map === 'undefined'){
-			let latLng = new google.maps.LatLng(35.6812405, 139.7649361); //東京駅
-			let mapOptions = {
-				zoom: 15,
-				center: latLng,
-				mapId: "574de86c3981bebd"
-			};
-			map = new google.maps.Map(document.getElementById('map'), mapOptions); 	// マップがまだ存在しない場合は新しく作成
-		}
-		
-	}
-	
-	const options = {
-	  enableHighAccuracy: true, //精度（trueだと良い）
-	  timeout: 5000, //制限時間
-	  maximumAge: 0, //キャッシュの位置情報の有効期限。期限内だと新たに取得せずキャッシュから返す
-	};
-	
 	navigator.geolocation.watchPosition(success, fail, options); // 現在地が取得できればsuccessに位置情報、できなければfail関数にエラーを代入、optionsは各種設定
 }
+
+function success(position) {
+	currentLatlng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude); // 現在地のlatlngオブジェクトを更新
+	document.getElementById('current-latlng').value = currentLatlng;  //店舗検索の中心地として、フォームのhiddenで送る現在地の更新
+	currentLocationInfoStatusDiv.style.display = 'none';
+	
+	if(typeof map === 'undefined'){
+		const mapOptions = {
+			zoom: 18,
+			center: currentLatlng,
+			mapId: "574de86c3981bebd"
+		};
+		map = new google.maps.Map(document.getElementById('map'), mapOptions); 	// マップがまだ存在しない場合は新しく作成
+	}
+	
+	if (typeof currentLocationMarker === 'undefined') { // マーカーがまだ存在しない場合は新しく作成
+		const currentLocationMarkerDiv = document.createElement('div');
+		currentLocationMarkerDiv.id = 'current-location-marker';
+		currentLocationMarker = new google.maps.marker.AdvancedMarkerView({
+            map,
+			position: currentLatlng,
+			content: currentLocationMarkerDiv,
+		});
+		
+		currentLocationCircle = new google.maps.Circle({
+			//stroke:ライン
+            strokeColor: '#115EC3', //青
+            strokeOpacity: 0.2,
+            strokeWeight: 1, //幅
+            //fill:塗りつぶし
+            fillColor: '#115EC3',
+            fillOpacity: 0.2,
+            map: map,
+            center: currentLatlng,
+            radius: 40
+        });
+	} else { 							// マーカーが存在する場合は位置を更新
+		currentLocationMarker.position = currentLatlng;
+		currentLocationCircle.setCenter(currentLatlng);
+		currentLocationCircle.setVisible = true;
+	}
+}
+	
+function fail(error) {
+	currentLocationInfoStatusDiv.style.display = 'block';
+	currentLocationCircle.setVisible = false;
+	if(typeof map === 'undefined'){
+		const tokyoStationLatLng = new google.maps.LatLng(35.6812405, 139.7649361); //東京駅
+		const mapOptions = {
+			zoom: 15,
+			center: tokyoStationLatLng,
+			mapId: "574de86c3981bebd"
+		};
+		map = new google.maps.Map(document.getElementById('map'), mapOptions); 	// マップがまだ存在しない場合は新しく作成
+	}
+}
+
+const options = {
+  enableHighAccuracy: true, //精度（trueだと良い）
+  timeout: 5000, //制限時間
+  maximumAge: 0, //キャッシュの位置情報の有効期限。期限内だと新たに取得せずキャッシュから返す
+};
 
 //		====== 検索フォーム ======
 //		------ ブランド名・予算セレクトボックスのテキストの色変更 ------
@@ -171,14 +168,14 @@ function storeSuccess(response) {
 		bounds.extend({lat: obj.lat, lng: obj.lng}); //矩形領域に各店舗の位置を追加
     });
 	//document.getElementById('back-to-list-btn').remove(); // 既存のボタンを削除
-	document.querySelector('#menu-board-container').style.display = "block"; // メニュー検索の予算設定セレクトボックスを表示
+	document.querySelector('#menu-board-container').style.display = 'block'; // メニュー検索の予算設定セレクトボックスを表示
 	let menuResultContainer = document.getElementById('menu-result-container');
 	while (menuResultContainer.firstChild) {
 	  menuResultContainer.removeChild(menuResultContainer.firstChild);
 	}
 	
-	if(currentLatLng !== 'undefined'){
-		bounds.extend(currentLatLng); //現在地が取得できていれば矩形領域に追加
+	if(currentLatlng !== 'undefined'){
+		bounds.extend(currentLatlng); //現在地が取得できていれば矩形領域に追加
 	}
 	map.fitBounds(bounds); //マップに矩形領域を伝える
 	
@@ -195,7 +192,7 @@ function calcRoute(obj) {
     let preSibling = obj.previousElementSibling;
     let desLatLng = new google.maps.LatLng(preSibling.value, preSibling.getAttribute('name')); // hiddenにある目的地の経緯度を取得
     let request = {
-        origin: currentLatLng, // 現在地の経緯度
+        origin: currentLatlng, // 現在地の経緯度
         destination: desLatLng,
         travelMode: 'WALKING'
     };
@@ -218,7 +215,7 @@ function backToList(){
 	directionsRenderer.setMap(null); // ルート案内を消すためにレンダラとマップの関連を削除
 	
     bounds = new google.maps.LatLngBounds(); // 最新の現在地のみを保持するために矩形領域をリセット
-    bounds.extend(currentLatLng); // 最新の現在地を追加
+    bounds.extend(currentLatlng); // 最新の現在地を追加
     markers.forEach(marker => {
         marker.map = map; // 各店舗のマーカーを再表示
 		bounds.extend(marker.position); //各店舗の位置を再追加
